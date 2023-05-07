b0f14d1ccb02b8f239631957dbc10be94d0a6faf
Yuri Paiva <yuriqpaiva@gmail.com>
2023-05-07 20:53:31 -0300

    Add products slider styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,55 @@
-import { styled } from '@/styles';
+import { HomeContainer, Product } from '@/styles/pages/home';
+import Image from 'next/image';
+import camiseta1 from '../assets/camisetas/1.png';
+import camiseta2 from '../assets/camisetas/2.png';
+import camiseta3 from '../assets/camisetas/3.png';
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  },
-});
+import { useKeenSlider } from 'keen-slider/react';
+import 'keen-slider/keen-slider.min.css';
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
